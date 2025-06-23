c086a079bb9a26fb8bb6b3d902353b8e364a9354
Felipe Lara <laralimabilly@gmail.com>
2025-06-23 10:45:40 -0300

    Fix astro config for storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
-import storyblok from '@storyblok/astro';
+import { storyblok } from '@storyblok/astro';
 
 export default defineConfig({
   site: 'http://dayonetalent.com',
